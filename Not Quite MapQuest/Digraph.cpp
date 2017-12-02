47fb86af7ffb6ee9801f2e32ff1a319b731a1a2c
Derrick Patterson <dlp70@zips.uakron.edu>
2017-12-02 10:32:27 -0500

    Implemented resetEdges(), addEdge(), delEdge(), and isEdge()

--- Not Quite MapQuest/Digraph.cpp
+++ Not Quite MapQuest/Digraph.cpp
@@ -1,6 +1,7 @@
 // Digraph.cpp -- directed graph class
 // c. 2017 T. O'Neil
 
+#include <climits>
 #include "Digraph.hpp"
 
 unsigned int Digraph::noVertices() {
@@ -12,19 +13,27 @@ unsigned int Digraph::noEdges() {
 }
 
 void Digraph::resetEdges() {
-
+	// Fill each vector element with max int value to represent infinity
+	for (int i = 0; i < numberOfVertices; i++)
+		for (int j = 0; j < numberOfVertices; j++)
+			distMatrix[i][j] = INT_MAX;
 }
 
 void Digraph::addEdge(int source, int dest, int wt) {
-
+	// Add distance to distance matrix; increment number of edges 
+	distMatrix[source][dest] = wt;
+	numberOfEdges++;
 }
 
 void Digraph::delEdge(int source, int dest) {
-
+	// Remove distance from distance matrix; decrement number of edges
+	distMatrix[source][dest] = INT_MAX;
+	numberOfEdges--;
 }
 
 int Digraph::isEdge(int source, int dest) {
-
+	// Find and return distance from distance matrix
+	return distMatrix[source][dest];
 }
 
 int Digraph::dijkstra(int source, int dest) {
