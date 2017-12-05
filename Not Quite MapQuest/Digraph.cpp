f93cd5e127fdfa6f0791dc7cac78b59765e42bc0
Derrick Patterson <dlp70@zips.uakron.edu>
2017-12-05 16:50:24 -0500

    "Implemented" dijkstra().

--- Not Quite MapQuest/Digraph.cpp
+++ Not Quite MapQuest/Digraph.cpp
@@ -5,10 +5,12 @@
 #include "Digraph.hpp"
 
 unsigned int Digraph::noVertices() {
+	// Return total number of verticies
 	return numberOfVertices;
 }
 
 unsigned int Digraph::noEdges() {
+	// Return total number of edges
 	return numberOfEdges;
 }
 
@@ -16,21 +18,18 @@ void Digraph::resetEdges() {
 	// Fill each vector element with max int value to represent infinity
 	for (int i = 0; i < numberOfVertices; i++)
 		for (int j = 0; j < numberOfVertices; j++)
-			if (i == j) distMatrix[i][j] = 0;
-			else distMatrix[i][j] = INT_MAX;
+			distMatrix[i][j] = INT_MAX;
 }
 
 void Digraph::addEdge(int source, int dest, int wt) {
 	// Add distance to distance matrix; increment number of edges 
-	if (source == dest) distMatrix[source][dest] = 0;
-	else distMatrix[source][dest] = wt;
+	distMatrix[source][dest] = wt;
 	numberOfEdges++;
 }
 
 void Digraph::delEdge(int source, int dest) {
 	// Remove distance from distance matrix; decrement number of edges
-	if (source == dest) distMatrix[source][dest] = 0;
-	else distMatrix[source][dest] = INT_MAX;
+	distMatrix[source][dest] = INT_MAX;
 	numberOfEdges--;
 }
 
@@ -41,4 +40,66 @@ int Digraph::isEdge(int source, int dest) {
 
 int Digraph::dijkstra(int source, int dest) {
 
+	std::vector<bool> path(numberOfVertices);
+	std::vector<int> distance(numberOfVertices);
+	int min, min_index;
+					
+	for (int i = 0; i < numberOfVertices; i++)
+		distance[i] = INT_MAX, path[i] = false;
+
+	distance[source] = 0;
+
+	for (int count = 0; count < numberOfVertices - 1; count++)	{
+		for (int v = 0; v < numberOfVertices; v++)
+			if (path[v] == false && distance[v] <= INT_MAX)
+				min = distance[v], min_index = v;
+
+		path[min_index] = true;
+
+		for (int v = 0; v < numberOfVertices; v++)
+			if (!path[v] && distMatrix[min_index][v] && distance[min_index] != INT_MAX
+				&& distance[min_index] + distMatrix[min_index][v] < distance[v])
+				distance[v] = distance[min_index] + distMatrix[min_index][v];
+	}
+
+	for (int i = 0; i < distance.size(); i++)
+		cout << "distance[" << i << "] = " << distance[i] << endl;
+
+	return distance[dest];
 }
+
+//int Digraph::dijkstra(int source, int dest) {
+//
+//	std::vector<int> distance(numberOfVertices);
+//	std::vector<int> previous(numberOfVertices);
+//	int minDistance = INT_MAX;
+//	int closestVertex;
+//	int count = 0;
+//
+//	vertex[source]->setStatus(VISITED);
+//
+//	for (int i = 0; i < numberOfVertices; i++)
+//		distance[i] = INT_MAX;
+//
+//	while (count < numberOfVertices) {
+//		for (int i = 0; i < numberOfVertices; i++) {
+//			if ((vertex[i]->getStatus() == NOT_VISITED) && (minDistance >= distance[i])) {
+//				minDistance = distance[i];
+//				closestVertex = i;
+//			}
+//		}
+//
+//		vertex[closestVertex]->setStatus(VISITED);
+//
+//		for (int i = 0; i < numberOfVertices; i++) {
+//			if (vertex[i]->getStatus() == NOT_VISITED) {
+//				if (distance[i] > distance[closestVertex] + distMatrix[closestVertex][i]) {
+//					distance[i] = distance[closestVertex] + distMatrix[closestVertex][i];
+//					previous[i] = closestVertex;
+//				}
+//			}
+//		}
+//		count++;
+//	}
+//	return distance[dest];
+//}
