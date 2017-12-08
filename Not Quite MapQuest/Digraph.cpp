59a1693130343a94eaa81db084ae8068607d38d7
Derrick Patterson <dlp70@zips.uakron.edu>
2017-12-08 18:57:06 -0500

    Added more comments; changed some syntax in dijkstra()

--- Not Quite MapQuest/Digraph.cpp
+++ Not Quite MapQuest/Digraph.cpp
@@ -1,7 +1,6 @@
 // Digraph.cpp -- directed graph class
 // c. 2017 T. O'Neil
 
-#include <climits>
 #include "Digraph.hpp"
 
 #define INF 2000000000
@@ -17,7 +16,7 @@ unsigned int Digraph::noEdges() {
 }
 
 void Digraph::resetEdges() {
-	// Fill each vector element with max int value to represent INF9inity
+	// Fill each vector element with a zero or INF (i.e. no edges)
 	for (int i = 0; i < numberOfVertices; i++)
 		for (int j = 0; j < numberOfVertices; j++)
 			if (i == j) distMatrix[i][j] = 0;
@@ -25,56 +24,55 @@ void Digraph::resetEdges() {
 }
 
 void Digraph::addEdge(int source, int dest, int wt) {
-	// Add shortestPathance to shortestPathance matrix; increment number of edges 
+	// Add edge to adjacency matrix; increment number of edges 
 	if (source == dest) distMatrix[source][dest] = 0;
 	else distMatrix[source][dest] = wt;
 	numberOfEdges++;
 }
 
 void Digraph::delEdge(int source, int dest) {
-	// Remove shortestPathance from shortestPathance matrix; decrement number of edges
+	// Remove edge from adjadency matrix; decrement number of edges
 	if (source == dest) distMatrix[source][dest] = 0;
 	else distMatrix[source][dest] = INF;
 	numberOfEdges--;
 }
 
 int Digraph::isEdge(int source, int dest) {
-	// Find and return shortestPathance from shortestPathance matrix
+	// Find and return edge between two verticies
 	return distMatrix[source][dest];
 }
 
 int Digraph::dijkstra(int source, int dest) {
-	// Vector to hold shortest path from source to dest 
+	// Vector to hold shortest path from source to dest, initialized to INF
 	std::vector<int> shortestPath(numberOfVertices, INF);
 
 	// Source has distance of zero
 	shortestPath[source] = 0;
 
 	// Find shortest path for all verticies
-	for (int count = 0; count < numberOfVertices - 1; count++) {
+	for (int i = 0; i < numberOfVertices - 1; i++) {
+		int min = INF, v;
 
-		int min = INF, minIndex;
-
-		// Find the vertex with the shortest distance
-		for (int i = 0; i < numberOfVertices; i++)
-			if (vertex[i]->getStatus() == NOT_VISITED && shortestPath[i] <= min) {
-				min = shortestPath[i];
-				minIndex = i;
+		// Find the next vertex with the shortest distance
+		for (int j = 0; j < numberOfVertices; j++) {
+			if (vertex[j]->getStatus() == NOT_VISITED && shortestPath[j] <= min) {
+				min = shortestPath[j];
+				// v <- index to shortest path
+				v = j;
 			}
-		// Update index for shortestPath
-		int j = minIndex;
+		}
 		// Vertex marked as VISITED
-		vertex[j]->setStatus(VISITED);
+		vertex[v]->setStatus(VISITED);
 
 		// Update distance values at new vertex
-		for (int i = 0; i < numberOfVertices; i++)
+		for (int w = 0; w < numberOfVertices; w++) {
 			// If i has not been visited, is connected to the vertex, and is less than shortestPath so far...
-			if (vertex[i]->getStatus() == NOT_VISITED && distMatrix[j][i] && shortestPath[j] != INF
-				&& shortestPath[j] + distMatrix[j][i] < shortestPath[i])
+			if (vertex[w]->getStatus() == NOT_VISITED && isEdge(w, v) && shortestPath[v] != INF
+				&& shortestPath[v] + isEdge(v, w) < shortestPath[w])
 				// Update shortestPath with new distance
-				shortestPath[i] = shortestPath[j] + distMatrix[j][i];
+				shortestPath[w] = shortestPath[v] + isEdge(v, w);
+		}
 	}
-
 	// Reset all verticies back to NOT_VISITED
 	for (int i = 0; i < vertex.size(); i++)
 		vertex[i]->setStatus(NOT_VISITED);
